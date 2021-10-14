b3ab25b658bf8b0d018925f02f5deb87d586d7d0
Oleh0293 <oleh.kulyk.ik.2021@lpnu.ua>
2021-10-14 18:01:17 +0300

    first commit

---
+++ lab 4.7.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    double xp, xk, x, dx, eps, a = 0, R = 0, S = 0;
+    int n;
+    cout << "xp = "; cin >> xp;
+    cout << "xk = "; cin >> xk;
+    cout << "dx = "; cin >> dx;
+    cout << "eps = "; cin >> eps;
+
+    cout << fixed;
+    cout << "-------------------------------------------------" << endl;
+    cout << "|" << setw(7) << "x" << " |"
+         << setw(10) << "-exp(x)" << " |"
+         << setw(10) << "S" << " |"
+         << setw(5) << "n" << " |"
+         << endl;
+    cout << "-------------------------------------------------" << endl;
+    x = xp;
+
+    while (x <= xk)
+    {
+        n = 0;
+        a = -1./x;
+        S = a;
+        do {
+            n++;
+            R = (-(2*n-1)/(2*n+1))*1./pow(x,2) ;
+            a *= R;
+            S += a;
+        } while (abs(a) >= eps);
+        cout << "|" << setw(7) << setprecision(2) << x << " |"
+             << setw(10) << setprecision(3) << atan(x) << " |"
+             << setw(10) << setprecision(3) << S << " |"
+             << setw(5) << n << " |"
+             << endl;
+        x += dx;
+    }
+    cout << "-------------------------------------------------" << endl;
+
+    return 0;
+}
